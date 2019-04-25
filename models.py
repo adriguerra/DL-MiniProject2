68fe93157c9109b20e144a7a48c6bff699c90cde
Adrian Guerra Alejos <adrian.guerra@epfl.ch>
2019-04-25 17:37:48 +0200

    Add models.py

---
+++ models.py
@@ -0,0 +1,23 @@
+import torch
+import functions
+
+class Linear(torch.nn.Module):
+"""Applies a linear transformation to incoming data"""
+
+    def __init__(self, in_features, out_features, bias=True):
+            super(Linear, self).__init__()
+            self.in_features = in_features
+            self.out_features = out_features
+            self.weight = torch.Tensor(out_features, in_features)
+            if bias:
+                self.bias = torch.Tensor(out_features)
+            else:
+                self.bias = None
+            # self.reset_parameters()
+    def forward(self, *input):
+        return linear(input, self.weight, self.bias)
+
+    def backward(self, *gradwrtoutput):
+        raise NotImplementedError
+
+    def param(self):
