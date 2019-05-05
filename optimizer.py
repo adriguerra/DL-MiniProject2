92c87aabcf26922c2e8dd76143741e07562c96d6
Adrian Guerra Alejos <adrian.guerra@epfl.ch>
2019-05-05 13:15:07 +0200

    Implement SGD optimizer

---
+++ optimizer.py
@@ -0,0 +1,30 @@
+class SGD(object):
+    """Implements stochastic gradient descent"""
+    def __init__(self, params, lr):
+        if lr < 0.0:
+            raise ValueError("Invalid learning rate: {}".format(lr))
+
+        self.lr = lr
+
+        if isinstance(params, torch.Tensor):
+            raise TypeError("params argument given to the optimizer should be "
+                            "an iterable of Tensors or dicts, but got " +
+                            torch.typename(params))
+
+        if len(params) == 0:
+            raise ValueError("optimizer got an empty parameter list")
+
+        self.params = params
+
+    def step(self):
+        """Performs a single optimization step."""
+        for (p, grad) in self.params:
+            p = p - (self.lr * grad)
+
+    def zero_grad(self):
+        r"""Clears the gradients of all optimized :class:`torch.Tensor` s."""
+        for group in self.param_groups:
+            for p in group['params']:
+                if p.grad is not None:
+                    p.grad.detach_()
+                    p.grad.zero_()
