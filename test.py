a1c3e1111e9a45cbbd4ad69b15a714dbe6ebd09b
Adrian Guerra Alejos <adrian.guerra@epfl.ch>
2019-05-08 13:03:13 +0200

    Convert to one-hot encoded outputs in test.py

--- test.py
+++ test.py
@@ -6,4 +6,10 @@ def generate_disc_set(nb):
     target = input.pow(2).sum(1).sub(1 / (2 * math.pi)).sign().add(1).div(2).long()
     return input, target
 
+def convert_to_one_hot_labels(input, target):
+    tmp = input.new_zeros(target.size(0), target.max() + 1)
+    tmp.scatter_(1, target.view(-1, 1), 1.0)
+    return tmp
+
 train_input, train_target = generate_disc_set(1000)
+train_target = convert_to_one_hot_labels(train_input, train_target)
