a0fec629ff91341f1ed2ef9a3f3e83963514011d
Adrian Guerra Alejos <adrian.guerra@epfl.ch>
2019-05-14 11:27:08 +0200

    Reorder methods in functions.py

--- functions.py
+++ functions.py
@@ -6,12 +6,6 @@ def tanh(x):
 def dtanh(x):
     return 4 * (x.exp() + x.mul(-1).exp()).pow(-2)
 
-def loss(v, t):
-    return (v - t).pow(2).sum()
-
-def dloss(v, t):
-    return 2 * (v - t)
-
 def relu(x):
     return torch.max(x, torch.empty(x.size()))
 
@@ -19,6 +13,12 @@ def drelu(x):
     ones = torch.ones(x.shape)
     zeros = torch.zeros(x.shape)
     return torch.where((x > 0), ones, zeros)
+
+def loss(v, t):
+    return (v - t).pow(2).sum()
+
+def dloss(v, t):
+    return 2 * (v - t)
 
 def linear(input, weights, bias=None):
     """Applies a linear transformation to incoming data"""
