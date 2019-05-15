4285e5b5e032b2c0e298a9dd15b6858c8b8686db
Adrian Guerra Alejos <adrian.guerra@epfl.ch>
2019-05-15 12:42:40 +0200

    Use architecture object in optimizer to update weights and bias

--- optimizer.py
+++ optimizer.py
@@ -1,29 +1,27 @@
 import torch
+import modules
 
 class SGD(object):
     """Implements stochastic gradient descent"""
-    def __init__(self, params, lr):
+    def __init__(self, arch, lr):
         if lr < 0.0:
             raise ValueError("Invalid learning rate: {}".format(lr))
 
         self.lr = lr
 
-        if isinstance(params, torch.Tensor):
-            raise TypeError("params argument given to the optimizer should be "
-                            "an iterable of Tensors or dicts, but got " +
-                            torch.typename(params))
+        if not isinstance(arch, modules.Module):
+            raise TypeError("Architecture given to the optimizer should be "
+                            "an instance of a module, but got " +
+                            torch.typename(arch))
 
-        if len(params) == 0:
-            raise ValueError("optimizer got an empty parameter list")
-
-        self.params = params
+        self.arch = arch
 
     def step(self):
         """Performs a single optimization step."""
-        for (p, grad) in self.params:
-            p -= (self.lr * grad)
+        for (p, grad) in self.arch.param():
+            p.data -= (self.lr * grad)
 
     def zero_grad(self):
         """Clears the gradients of all Tensors."""
-        for (p, grad) in self.params:
-            grad = torch.zeros(grad.shape)
+        for (p, grad) in self.arch.param():
+            grad.data = torch.zeros(grad.shape)
