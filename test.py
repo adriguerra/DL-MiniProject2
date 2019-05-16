771bac47ace4bcc63b8504e1fb83f01c34943bf4
Adrian Guerra Alejos <adrian.guerra@epfl.ch>
2019-05-16 09:43:57 +0200

    Move generate data functions to helpers.py

--- test.py
+++ test.py
@@ -1,15 +1,11 @@
 import torch
 import math
+from helpers import generate_disc_set
+from helpers import convert_to_one_hot_labels
+from optimizer import SGD
+import modules
 
-def generate_disc_set(nb):
-    input = torch.Tensor(nb, 2).uniform_(0, 1)
-    target = input.pow(2).sum(1).sub(1 / (2 * math.pi)).sign().add(1).div(2).long()
-    return input, target
-
-def convert_to_one_hot_labels(input, target):
-    tmp = input.new_zeros(target.size(0), target.max() + 1)
-    tmp.scatter_(1, target.view(-1, 1), 1.0)
-    return tmp
+torch.set_grad_enabled(False)
 
 # Generate data
 train_input, train_target = generate_disc_set(1000)
