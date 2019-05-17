0a846bf2de31c1f1a540126a28ba1782ea7d7f8a
Adrian Guerra Alejos <adrian.guerra@epfl.ch>
2019-05-17 20:10:07 +0200

    Print accuracy test.py

--- test.py
+++ test.py
@@ -45,7 +45,7 @@ model2 = modules.Sequential(modules.Linear(2, 25),
                            )
 
 # Define training parameters
-nb_epochs = 50
+nb_epochs = 1000
 lr = 1e-5
 mini_batch_size = 100
 optimizer1 = SGD(model1.param(), lr=lr)
@@ -57,14 +57,12 @@ print("Training model 1")
 # Train model 1
 for e in range(nb_epochs):
     sum_loss = 0
-    nb_err1 = 0
     for b in range(0, train_input.size(0), mini_batch_size):
         output, loss = model1.forward(train_input.narrow(0, b, mini_batch_size), train_target.narrow(0, b, mini_batch_size))
         optimizer1.zero_grad()
         grad = model1.backward()
         sum_loss = sum_loss + loss.item()
         optimizer1.step()
-        nb_err1 = nb_err1 + compute_nb_errors(output, train_target.narrow(0, b, mini_batch_size))
 
     print("Iteration {0:}: loss = {1:.3f}".format(e+1, sum_loss / (train_input.shape[0]/mini_batch_size)),
           end='\r',
@@ -83,8 +81,7 @@ nb_err_test1 = compute_nb_errors(output_test1, test_target)
 print("Model 1 with TanH results")
 print("Training loss: {:.3f}".format(loss_train1))
 print("Test loss: {:.3f}".format(loss_test1))
-print("Number of errors on train set: {:.2f}%".format((100*(nb_err1/train_target.shape[0]))))
-print("Number of errors on test set: {:.2f}%".format((100*(nb_err_test1/train_target.shape[0]))))
+print("Accuracy: {:.2f}%".format(100 - (100*(nb_err_test1/train_target.shape[0]))))
 
 
 print("#" * 50)
@@ -93,14 +90,12 @@ print("Training model 2")
 # Train model 2
 for e in range(nb_epochs):
     sum_loss = 0
-    nb_err2 = 0
     for b in range(0, train_input.size(0), mini_batch_size):
         output, loss = model2.forward(train_input.narrow(0, b, mini_batch_size), train_target.narrow(0, b, mini_batch_size))
         optimizer2.zero_grad()
         grad = model2.backward()
         sum_loss = sum_loss + loss.item()
         optimizer2.step()
-        nb_err2 = nb_err2 + compute_nb_errors(output, train_target.narrow(0, b, mini_batch_size))
 
     print("Iteration {0:}: loss = {1:.3f}".format(e+1, sum_loss / (train_input.shape[0]/mini_batch_size)),
           end='\r',
@@ -118,6 +113,4 @@ nb_err_test2 = compute_nb_errors(output_test2, test_target)
 print("Model 2 with ReLu results")
 print("Training loss: {:.3f}".format(loss_train2))
 print("Test loss: {:.3f}".format(loss_test2))
-
-print("Number of errors on train set: {:.2f}%".format((100*(nb_err2/train_target.shape[0]))))
-print("Number of errors on test set: {:.2f}%".format((100*(nb_err_test2/train_target.shape[0]))))
+print("Accuracy: {:.2f}%".format(100 - (100*(nb_err_test2/train_target.shape[0]))))
